bb66693385cbcb4db804b3271c2c1550046087de
GaYeonLEE <gyleeforyt@gmail.com>
2024-05-20 02:40:46 +0900

    민원 게시판

--- villive_user/app/src/main/java/com/example/villive/Community/Community_Complain.kt
+++ villive_user/app/src/main/java/com/example/villive/Community/Community_Complain.kt
@@ -1,17 +1,26 @@
 package com.example.villive.Community
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.villive.Community_Write.Post_Complain
+import com.example.villive.Post_model_adapter.Complain
+import com.example.villive.Post_model_adapter.ComplainAdapter
+import com.example.villive.Post_model_adapter.PostAdapter
+import com.example.villive.Post_model_adapter.Posts
 import com.example.villive.R
 
 class Community_Complain : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -21,6 +30,31 @@ class Community_Complain : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        // 일단 임시로 값 넣어뒀음
+        val complainlist= arrayListOf(
+            Complain("쓰레기좀","아무데나 버리지 마세요","가연","09:03","환경 개선"),
+            Complain("화단에","담배꽁초가 너무 많은거같아요","가연","10:01","환경 개선"),
+            Complain("공동현관 자동문","고장났어요","가연","10:26","공동 시설"),
+            Complain("집 창문에","벌집이 있어요","가연","10:40","기타 민원"),
+            Complain("화단에","쓰레기가 많아요","가연","11:07","환경 개선"),
+            Complain("엘베","에어컨에서 물떨어져요","가연","11:53","기계 고장"),
+            Complain("엘베에","전단지 붙여요","가연","12:04","공동 시설"),
+            Complain("정원꾸미기","주민들이랑 하면 좋을거같아요 ~ ^^","가연","13:47","환경 개선"),
+            Complain("외부인 주차","차단기 설치 해주세요","가연","13:55","건의 사항"),
+
+            )
+
+        val rv_complain_post = findViewById<RecyclerView>(R.id.rv_posts_complain)
+
+        rv_complain_post.layoutManager= LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false)
+        rv_complain_post.setHasFixedSize(true)
+
+        rv_complain_post.adapter= ComplainAdapter(complainlist)
+
+
+
 
         val notice = findViewById<ImageButton>(R.id.ibtn_notice)
 
---
+++ villive_user/app/src/main/java/com/example/villive/Post_model_adapter/Complain.kt
@@ -0,0 +1,3 @@
+package com.example.villive.Post_model_adapter
+
+class Complain (val title: String, val write: String, val nickname: String, val write_time: String, val complain_kind: String)
