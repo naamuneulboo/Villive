fcf4b479d8ddc0d28560974d07ea486d68738720
GaYeonLEE <gyleeforyt@gmail.com>
2024-06-05 22:26:57 +0900

    회원 탈퇴
    * 회원 탈퇴 후 StartPage로 이동

--- villive_user/app/src/main/java/com/example/villive/ConfigurationPage/UnregisterActivity.kt
+++ villive_user/app/src/main/java/com/example/villive/ConfigurationPage/UnregisterActivity.kt
@@ -1,13 +1,16 @@
 package com.example.villive.ConfigurationPage
+
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.villive.R
-import com.example.villive.Retrofit.DeleteMemRequestDtoAPI
+import com.example.villive.Retrofit.MemberDeleteApi
 import com.example.villive.Retrofit.RetrofitService
-import com.example.villive.model.MsgResponseDto
+import com.example.villive.User_SignPage.StartPage
 import com.example.villive.model.UnregisterRequestDto
 import okhttp3.ResponseBody
 import retrofit2.Call
@@ -16,43 +19,63 @@ import retrofit2.Response
 
 class UnregisterActivity : AppCompatActivity() {
 
-    private lateinit var deleteMemRequestDtoAPI: DeleteMemRequestDtoAPI
+    private lateinit var passwordEditText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.unregister)
 
-        val passwordEditText = findViewById<EditText>(R.id.editTextPassword)
+        passwordEditText = findViewById(R.id.editTextPassword)
         val unregisterButton = findViewById<Button>(R.id.buttonUnregister)
-
-        // Retrofit 객체 생성
-        val retrofit = RetrofitService.getService(this)
-        deleteMemRequestDtoAPI = retrofit.create(DeleteMemRequestDtoAPI::class.java)
 
         unregisterButton.setOnClickListener {
             val password = passwordEditText.text.toString()
-
-            // 입력된 비밀번호를 사용하여 회원 탈퇴 요청 생성
-            val unregisterRequestDto = UnregisterRequestDto(password)
-
-            // 회원 탈퇴 요청 수행
-            deleteMemRequestDtoAPI.deleteMember(unregisterRequestDto).enqueue(object : Callback<MsgResponseDto> {
-                override fun onResponse(call: Call<MsgResponseDto>, response: Response<MsgResponseDto>) {
-                    if (response.isSuccessful) {
-                        // 회원 탈퇴 성공
-                        Toast.makeText(this@UnregisterActivity, "회원 탈퇴되었습니다.", Toast.LENGTH_SHORT).show()
-                        finish() // 현재 액티비티 종료
-                    } else {
-                        // 회원 탈퇴 실패
-                        Toast.makeText(this@UnregisterActivity, "회원 탈퇴 실패: ${response.message()}", Toast.LENGTH_SHORT).show()
-                    }
-                }
-
-                override fun onFailure(call: Call<MsgResponseDto>, t: Throwable) {
-                    // 네트워크 오류 등의 실패 처리
-                    Toast.makeText(this@UnregisterActivity, "네트워크 오류: ${t.message}", Toast.LENGTH_SHORT).show()
-                }
-            })
+            if (password.isNotBlank()) {
+                showConfirmationDialog(password)
+            } else {
+                Toast.makeText(this, "비밀번호를 입력해주세요 !", Toast.LENGTH_SHORT).show()
+            }
         }
+    }
+
+    private fun showConfirmationDialog(password: String) {
+        val builder = AlertDialog.Builder(this)
+        builder.setMessage("정말 탈퇴하시겠습니까?")
+            .setPositiveButton("네") { dialog, which ->
+                unregisterMember(password)
+                dialog.dismiss()
+            }
+            .setNegativeButton("아니오") { dialog, which -> dialog.dismiss() }
+        val dialog = builder.create()
+        dialog.show()
+    }
+
+    private fun unregisterMember(password: String) {
+        val retrofit = RetrofitService.getService(this)
+        val memberDeleteApi = retrofit.create(MemberDeleteApi::class.java)
+        val request = UnregisterRequestDto(password)
+
+        val call = memberDeleteApi.deleteMember(request)
+
+        call.enqueue(object : Callback<ResponseBody> {
+            override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
+                if (response.isSuccessful) {
+                    Toast.makeText(this@UnregisterActivity, "회원 탈퇴가 완료되었습니다.\n빌리브를 이용해 주셔서 감사합니다.", Toast.LENGTH_SHORT).show()
+                    navigateToStartPage()
+                } else {
+                    Toast.makeText(this@UnregisterActivity, "회원 탈퇴 실패\n 사유: ${response.message()}", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                Toast.makeText(this@UnregisterActivity, "회원 탈퇴 에러: ${t.message}", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    private fun navigateToStartPage() {
+        val intent = Intent(this, StartPage::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
     }
 }
--- villive_user/app/src/main/java/com/example/villive/Retrofit/DeleteMemRequestDtoAPI.kt
+++ villive_user/app/src/main/java/com/example/villive/Retrofit/DeleteMemRequestDtoAPI.kt
@@ -1,13 +1,12 @@
 package com.example.villive.Retrofit
 
-import com.example.villive.model.MsgResponseDto
 import com.example.villive.model.UnregisterRequestDto
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.Body
-import retrofit2.http.DELETE
+import retrofit2.http.HTTP
 
-interface DeleteMemRequestDtoAPI {
-
-    @DELETE("/member/delMember")
-    fun deleteMember(@Body unregisterRequestDto: UnregisterRequestDto): Call<MsgResponseDto>
+interface MemberDeleteApi {
+    @HTTP(method = "DELETE", path = "/member/delMember", hasBody = true)  // delete에 body를 추가 해야할 경우 이렇게 !! 원래 delete에는 body가 못들어감
+    fun deleteMember(@Body request: UnregisterRequestDto): Call<ResponseBody>
 }
