5d57ee7c82370be1cadcac88af8b7abfba1025b0
지원영 <qkfka3521@gmail.com>
2024-06-09 07:03:43 +0900

    세입자관리, 설정, 관리비 사용내역 삭제
    + 기본 색상 보라색 -> 흰색 수정

--- villive_management/app/src/main/java/ood/villive_management/MGMT_main/MainActivity.kt
+++ villive_management/app/src/main/java/ood/villive_management/MGMT_main/MainActivity.kt
@@ -15,28 +15,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main) //xml 화면 뷰 연결
 
-        val tanent = findViewById<Button>(R.id.btn_tanent) // btn_tanent 버튼을 가져옴
         val notice = findViewById<Button>(R.id.btn_notice)
         val complain = findViewById<Button>(R.id.btn_complain)
-        val cost = findViewById<Button>(R.id.btn_cost)
-        val settings = findViewById<Button>(R.id.btn_settings)
 
-
-        // 버튼에 클릭 리스너 설정
-        tanent.setOnClickListener {
-            val intent = Intent(this@MainActivity, Tanent_mgmt::class.java) // 다음 화면으로 넘어가기 위한 intent 객체 생성
-            startActivity(intent)
-        }
         notice.setOnClickListener {
             val intent = Intent(this@MainActivity, NoticeList::class.java) // 다음 화면으로 넘어가기 위한 intent 객체 생성
-            startActivity(intent)
-        }
-        cost.setOnClickListener {
-            val intent = Intent(this@MainActivity, AddUsageHistory::class.java) // 다음 화면으로 넘어가기 위한 intent 객체 생성
-            startActivity(intent)
-        }
-        settings.setOnClickListener {
-            val intent = Intent(this@MainActivity, Settings::class.java) // 다음 화면으로 넘어가기 위한 intent 객체 생성
             startActivity(intent)
         }
         complain.setOnClickListener {
--- villive_management/app/src/main/java/ood/villive_management/Model/ComplainRequestDto.kt
+++
@@ -1,8 +0,0 @@
-package ood.villive_management.Model
-
-data class ComplainRequestDto(
-    val contents: String? = null,
-    val status: String? = null,
-    val title: String? = null,
-    val type: String? = null
-)
